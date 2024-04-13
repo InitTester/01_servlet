9545c78a43a38fe3d083286c4678f79beb1df450
InitTester <Initsave@gmail.com>
2024-04-14 01:07:14 +0900

    20240414 commit 1차

--- src/main/java/com/portfolio/www/com/controller/InOutController.java
+++ src/main/java/com/portfolio/www/com/controller/InOutController.java
@@ -1,11 +1,16 @@
 package com.portfolio.www.com.controller;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.portfolio.www.com.service.Calculation;
+
+@Controller
 public class InOutController {
 
 	 public InOutController() {		
@@ -20,10 +25,41 @@ public class InOutController {
 	
 	@RequestMapping("/output.do")
 	public ModelAndView output(@RequestParam HashMap<String, String> params) {
+
+		ModelAndView mv = new ModelAndView();
 		
+		if(params.get("measureDate").toString() == "") {
+			System.out.println("되돌아갈래ㅐㅐ");
+			mv.setViewName("input");
+			mv.addObject("error","날짜형식이 올바르지 않습니다.");
+			return mv;
+		}
+//		System.out.println("params.get(\"measureDate\") : " + params.get("measureDate"));
 		String measureDate = params.get("measureDate").toString();
+//		LocalDateTime measureDate = LocalDateTime.parse(params.get("measureDate").toString());
 		String eAmount = params.get("eAmount").toString();
-		/*여름철 구분 : 6월 1일 ~ 8월 31일 */
+		
+		
+		Calculation cal = Calculation.getInstance();
+		
+		cal.set사용량(Integer.parseInt(eAmount));
+		
+		int 단계 = cal.get사용량() <=200 ? 1 : (201 <= cal.get사용량() && cal.get사용량() < 400 ? 2 : 3);
+		String 기본요금 = String.format("%,d원 (%d단계 단가)",cal.get기본요금(),단계);
+		String 전력량요금 = String.format("%,d원 (%s단계 요금의 합계, 원미만절사)",(int)cal.get전력량요금(),1<단계? "1~"+단계 : 단계);
+		String 기후환경요금 = String.format("%,d kWh x 9원 = %,d원",(int)cal.get사용량(),(int)cal.get기후환경요금());
+		String 연료비조정요금 = String.format("%,d kWh x 5원 = %,d원",(int)cal.get사용량(),(int)cal.get연료비조정요금());
+		String 전기요금계 = String.format("%,d원 = %,d원(①) + %,d원(②) + %,d원(③)",(int)cal.get전기요금계(),cal.get기본요금(),(int)cal.get전력량요금(),(int)cal.get기후환경요금());
+//		System.out.println("현재 부가가치세 : " + (int)Math.round(cal.get부가가치세()));
+		String 부가가치세 = String.format("%,d원(⑤) x 10%% = %,d원 (원미만 반올림))",(int)cal.get전기요금계(),(int)Math.round(cal.get부가가치세()));
+		String 전력기반기금 = String.format("%,d원(⑤) x 3.7%% = %,d원 (10원미만절사))",(int)cal.get전기요금계(),(int)cal.get전력기반기금());
+//		System.out.println("청구 금액전 부가가치세 : " + (int)Math.round(cal.get부가가치세()));
+		String 청구금액 = String.format("%,d원(⑤) + %,d원(⑥) + %,d원(⑦) - 0원(⑧) = %,d원 (10원미만절사)",(int)cal.get전기요금계(),(int)Math.round(cal.get부가가치세()),(int)cal.get전력기반기금(),(int)(cal.get전기요금계() + cal.get부가가치세() + cal.get전력기반기금())/10 * 10);
+		
+//		int eAmount = Integer.valueOf(params.get("eAmount").toString());
+		
+		/*여름철 구분 : 6월 1일 ~ 8월 31일 
+		 *기초생활수급자 및 차상위 계층 정액할인은 정률할인(30%)과 중복 적용 가능 */
 		/*정액 할인
 		 * 1. 월 1만 6천원, 여름철 2만원 한도
 		 * 2. 월 1만 6천원, 여름철 2만원 한도
@@ -42,21 +78,21 @@ public class InOutController {
 		// 정률할인(30%)
 		String discountType2 = params.get("discountType2").toString();
 		
-		
-		
-		
-		
-
-		System.out.println(params);
-		System.out.println("measure : " + measureDate);
-		
-		
-		
-		ModelAndView mv = new ModelAndView();
 		mv.setViewName("output");
-		mv.addObject("measureDate", "2024-01-01");
-//		mv.addObject("measureDate", "2024-01-01");
-//		System.out.println(mv.getModelMap());
+		mv.addObject("measureDate", measureDate);
+		mv.addObject("eAmount", eAmount);
+		mv.addObject("step", 단계);
+		mv.addObject("basic", 기본요금);
+		mv.addObject("er", 전력량요금);
+		mv.addObject("cef", 기후환경요금);
+		mv.addObject("fcaf", 연료비조정요금);
+		mv.addObject("ebm", 전기요금계);
+		mv.addObject("vat", 부가가치세);
+		mv.addObject("eif", 전력기반기금);
+		mv.addObject("ba", 청구금액);
 		return mv;
 	}
+	
+	
+	
 }
---
+++ src/main/java/com/portfolio/www/com/service/Calculation.java
@@ -0,0 +1,141 @@
+package com.portfolio.www.com.service;
+
+public class Calculation {
+    // 6개의 요금을 구하는 메서드
+    // 싱글톤 패턴
+    private static Calculation instance;
+    private double[] items = {120.0d, 214.6d, 307.3d};
+
+    private double 사용량;
+    private int 기본요금;
+    private double 전력량요금;
+    private double 기후환경요금;
+    private double 연료비조정요금;
+    private double 전기요금계;
+    private double 부가가치세;
+    private double 전력기반기금;
+
+    private Calculation(){}
+
+    public static Calculation getInstance() {
+        if(instance == null){
+            synchronized (Calculation.class){
+                if(instance == null){
+                    instance = new Calculation();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public double get사용량() {
+        return 사용량;
+    }
+
+    public void set사용량(double 사용량) {
+        this.사용량 = 사용량;
+
+        set기본요금();
+        set전력량요금();
+        set기후환경요금();
+        set연료비조정요금();
+        set전기요금계();
+        set부가가치세();
+        set전력기반기금();
+    }
+
+    public int get기본요금() {
+        return 기본요금;
+    }
+
+    public void set기본요금() {
+        if(사용량 <=200) {기본요금 = 910;}
+        else if(201<= 사용량 && 사용량 <= 400 ) { 기본요금=1600;}
+        else if(사용량 > 400) { 기본요금 = 7300;}
+    }
+
+    public double get전력량요금() {
+        return 전력량요금;
+    }
+
+    public void set전력량요금() {
+    	int step = 사용량 <=200 ? 1 : (201 <= 사용량 && 사용량 < 400 ? 2 : 3);
+    	int 계산값 = (int)get사용량();
+    	
+    	for(int i=0; i <step; i++) {
+    		int 계산량 = Math.min(계산값, 200);
+    		계산값 -= 계산량;
+    		전력량요금 += (items[i] * 계산량);
+    		System.out.println(전력량요금);
+    	}
+//        if(0 <= 사용량-200){
+//            전력량요금 += 200 * 120;
+//        }
+//        if(0 <= 사용량-400){
+//            전력량요금 += 200 * 214.6;
+//        }
+//        if (0 < 사용량-400){
+//            전력량요금 += (사용량-400) * 307.3;
+//        }
+    }
+
+    public double get기후환경요금() {
+        return 기후환경요금;
+    }
+
+    public void set기후환경요금() {
+        기후환경요금 = 사용량 *9;
+    }
+    
+    public double get연료비조정요금() {
+        return 연료비조정요금;
+    }
+
+    public void set연료비조정요금() {
+    	연료비조정요금 = 사용량 *5;
+    }
+
+    public double get전기요금계() {
+        return 전기요금계;
+    }
+
+    public void set전기요금계() {
+        전기요금계 = (int)(get기본요금() + get전력량요금() +  get기후환경요금());
+    }
+
+    public double get부가가치세() {
+        return 부가가치세;
+    }
+
+    public void set부가가치세() {
+        부가가치세 = get전기요금계() * 0.1;
+    }
+
+    public double get전력기반기금() {
+        return 전력기반기금;
+    }
+
+    public void set전력기반기금() {
+        전력기반기금 = (int)(get전기요금계()*0.037/10) * 10;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("기본요금 : %,d원 \n" +
+                "전력량요금 : %,d원 \n" +
+                "기후환경요금 : %,d원 \n" +
+                "연료비조정요금 : %,d원 \n" +
+                "전기요금계 : %,d원 \n" +
+                "부가가치세 : %,d원 (원미만 반올림) \n" +
+                "전력기반기금 : %,d원 (10원미만절사) \n" +
+                "청구금액 : %,d원 (10원미만절사) \n",
+                기본요금,
+                (int)전력량요금,
+                (int)기후환경요금,
+                (int)연료비조정요금,
+                (int)전기요금계,
+                (int)Math.round(부가가치세),
+                (int)전력기반기금,
+                (int)(전기요금계 + 부가가치세 + 전력기반기금)/10 * 10) ;
+    }
+}
